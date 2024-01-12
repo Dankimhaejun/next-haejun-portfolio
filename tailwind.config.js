532712056da00482ee5ed78321fbe8a6debfc5e4
Dankimhaejun <dan.kimhaejun@gmail.com>
2024-01-12 14:19:38 +0900

    feat: Add tailwind.css, Set Header, Footer, Home

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  module: 'jit',
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class',
+  plugins: [],
+}
