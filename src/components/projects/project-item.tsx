62c045f624701b0a62032e16a6d82b5f2bd96a1d
Dankimhaejun <dan.kimhaejun@gmail.com>
2024-01-12 18:48:11 +0900

    feat - Create projects

---
+++ src/components/projects/project-item.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function ProjectItem({ data }) {
+  console.log('data :>> ', data)
+  const { Name, Video, Description, Tags, Created } = data.properties
+  const title = Name.title[0]?.plain_text
+  const video = Video?.url || 'https://github.com/Dankimhaejun'
+  const description = Description?.rich_text[0]?.plain_text
+  const cover = data.cover?.external.url
+  const tags = Tags?.multi_select
+  const created = Created?.date?.start
+
+  return (
+    <div className="project-card">
+      <Image
+        className="rounded-t-xl"
+        src={cover}
+        alt="cover image"
+        width={0}
+        height={0}
+        layout="responsive"
+        objectFit="cover"
+        quality={100}
+      />
+      <div className="p-4 flex flex-col">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <h3 className="mt-4 text-xl">{description}</h3>
+        <h3 className="mt-4 text-xl">{created}</h3>
+        <Link href={video}>비디오 보러가기</Link>
+        <div className="flex items-start mt-2">
+          {tags.map((tag, i) => (
+            <h1
+              key={i}
+              className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30"
+            >
+              {tag.name}
+            </h1>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,20 +1,61 @@
 import Layout from '@/components/layout'
+import ProjectItem from '@/components/projects/project-item'
 import Head from 'next/head'
 
-export default function Projects() {
+export default function Projects({ projects }) {
   return (
     <Layout>
-      <Head>
-        <title>Kim Hae Jun 포트폴리오</title>
-        <meta name="description" content="포트폴리오" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <section className="flex min-h-screen flex-col items-center justify-center body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          프로젝트
+      <div className="flex flex-col items-center justify-center min-h-screen py-24 mb-10 px-6">
+        <Head>
+          <title>Kim Hae Jun 포트폴리오</title>
+          <meta name="description" content="포트폴리오" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1 className="text-4xl font-bold sm:text-6xl">
+          총 프로젝트:
+          <span className="pl-4 text-blue-500">{projects.length}</span>
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 p-12 m-4 gap-8">
+          {projects.map((project, i) => (
+            <ProjectItem data={project} key={i} />
+          ))}
         </div>
-      </section>
+      </div>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  console.log('process.env.NOTION_DATABASE :>> ', process.env.NOTION_DATABASE)
+  console.log('process.env.NOTION_TOKEN :>> ', process.env.NOTION_TOKEN)
+
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${process.env.NOTION_DATABASE}/query`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${process.env.NOTION_TOKEN}`,
+        'Notion-Version': '2022-06-28',
+        'Content-Type': 'application/json',
+      },
+    },
+  )
+  const projects = await res.json()
+  // const res = await fetch(
+  //   `https://api.notion.com/v1/databases/${process.env.NOTION_DATABASE}`,
+  //   {
+  //     headers: {
+  //       Authorization: `Bearer ${process.env.NOTION_TOKEN}`,
+  //       'Notion-Version': '2022-06-28',
+  //       'Content-Type': 'application/json',
+  //     },
+  //   },
+  // )
+
+  return {
+    props: {
+      projects: projects.results,
+    },
+  }
 }
